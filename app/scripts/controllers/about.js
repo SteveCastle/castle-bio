a58750f17f6d5ee00fba03fb1ae8591af26b0484
Steve Castle <steve.castle@drssmail.com>
2014-06-22 12:29:15 -0700

    Ran yeoman angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name castleBioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the castleBioApp
+ */
+angular.module('castleBioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name castleBioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the castleBioApp
+ */
+angular.module('castleBioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
