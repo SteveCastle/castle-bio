587569d9d9fdabcbc8a4130ef6d6cc1c24695323
Steve Castle <steve@tigerhaunt.com>
2014-11-29 05:18:22 +1030

    Typo Fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,7 +12,7 @@ angular.module('castleBioApp')
     $scope.welcomeMessage = 'Harsh green light from a terminal floods your eyes..You seem to have been out cold sitting in a strange office.  You look up at the display squinting away as\
     much light as you can manage, your head is pounding, you cant remember the last time you had a headache.\
     On your display you see the records of a specific user, it seems to be a dosier. You cant remember bringing this file up or who this user Stephen Castle is.\
-      What happened to your memory, and why were you investigating this person? You resolve to dive deeper into the system to find the answer to these questions and questions and more.';
+      What happened to your memory, and why were you investigating this person? You resolve to dive deeper into the system to find the answer to these questions and more.';
     $scope.helpItems = [{'title':'cd <dir>', 'description': ''},
                        {'title':'mail --message=<msg>', 'description': ''},
                        ];
